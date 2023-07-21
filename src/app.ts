96028be1df2355c399efde8c4da06bd3cc31b249
Everton Vargas <evertonpassa7@gmail.com>
2023-07-21 08:03:36 -0300

    Chore: configuration Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Everton',
+    email: 'evertonpassa7@gmail.com',
+  },
+})
 
 export const app = fastify()
