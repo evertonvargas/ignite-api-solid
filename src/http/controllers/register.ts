c2d8340a8892be87b864af1f25456844ccf67083
Everton Vargas <evertonpassa7@gmail.com>
2023-07-22 15:56:16 -0300

    feat: handler error global

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -21,10 +21,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     await registerUseCase.execute({ name, email, password })
   } catch (err) {
     if (err instanceof UserAlreadyExistError) {
-      return reply.status(409).send()
+      return reply.status(409).send({ message: err.message })
     }
 
-    return reply.status(500).send()
+    throw err
   }
 
   return reply.status(201).send()
