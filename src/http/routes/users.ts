8a137f56b44b9325ccda3bdfff713c520043b13f
Everton Vargas <evertonpassa7@gmail.com>
2023-07-26 22:52:32 -0300

    feat: create middleware jwt

--- src/http/routes/users.ts
+++ src/http/routes/users.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { register } from '../controllers/register'
 import { authenticate } from '../controllers/authenticate'
 import { profile } from '../controllers/profile'
+import { verifyJwt } from '../middlewares/verify-jwt'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
 
   // User Authenticated
-  app.get('/profile', profile)
+  app.get('/profile', { onRequest: [verifyJwt] }, profile)
 }
